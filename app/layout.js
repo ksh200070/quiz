a9e041141f32444ea92b0c883afa525c88b306f0
kim seung hyun <skim@uroom.io>
2024-12-17 16:04:39 +0900

    Fix : favicon

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,9 @@ import { Analytics } from "@vercel/analytics/next";
 export const metadata = {
   title: "퀴즈 사이트 | 국기 맞추기",
   description: "여행과 관련한 다양한 퀴즈를 풀 수 있는 퀴즈 사이트입니다.",
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
